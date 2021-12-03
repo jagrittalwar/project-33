a0f59c927e2d807dff65c8e8145150603567a93f
jagrittalwar <87003559+jagrittalwar@users.noreply.github.com>
2021-12-03 15:46:29 +0530

    Add files via upload

---
+++ js/Question.js
@@ -0,0 +1,69 @@
+class Question {
+
+  constructor() {
+    this.title = createElement('h1')
+    this.input1 = createInput("Enter Your Name Here....");
+   //Create a input box to enter the number
+this.input2 = createInput("Enter The Correct Option Number")
+this.question = createElement('h3')
+this.option1 = createElement('h4')
+this.option2 = createElement('h4')
+this.option3 = createElement('h4')
+this.option4 = createElement('h4')
+    this.button = createButton('Submit');
+    //Create elements for One question (h3) and 4 options (h4)
+
+
+
+
+    this.message = createElement("h2")
+  }
+
+  hide(){
+     this.title.hide();
+    this.input1.hide();
+    this.button.hide();
+    this.input2.hide();
+    this.message.hide();
+
+
+
+
+  }
+
+  display(){
+    this.title.html("MyQuiz Game");
+    this.title.position(350, 0);
+
+
+    //Create html() and position() for each question, input and answers.
+this.question.html("What starts and ends with letter 'E',but has only one letter?")
+this.question.position(150,80)
+this.option1.html("Everyone")
+this.option1.position(150,100)
+this.option2.html("Envelope")
+this.option2.position(150,120)
+this.option3.html("Estimate")
+this.option3.position(150,140)
+this.option4.html("Example")
+this.option4.position(150,160)
+    this.input1.position(150, 230);
+    this.input2.position(350,230)
+    this.button.position(290, 300);
+    this.button.mousePressed(()=>{
+this.title.hide();
+this.input1.hide();
+this.input2.hide();
+this.button.hide();
+
+contestant.name = this.input1.value();
+contestant.answer = this.input2.value();
+contestantCount+=1
+contestant.index = contestantCount
+contestant.update();
+conntestant.updateCount(contestantCount);
+    })
+
+
+  }
+}
